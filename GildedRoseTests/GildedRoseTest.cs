c8fcda2c5ab87f6ba23d7cc8b3d1805704483e72
tykim83 <agate.fabrizio83@gmail.com>
2024-08-23 12:24:04 +0100

    Add project files.

---
+++ GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,18 @@
+﻿using GildedRoseKata;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GildedRoseTests
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal("foo", Items[0].Name);
+        }
+    }
+}
